6a2331aacd46a1e00e6795686eecaf88a8f4f0f2
Robin <robin.steil@gmx.de>
2023-03-13 18:07:29 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
+
+const app = createApp(App)
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
